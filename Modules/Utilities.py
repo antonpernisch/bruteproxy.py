4d7cd4c23598e17f0ae4fbbdcf93b7cfad933be3
esec's expolits <76219308+esec-exploits@users.noreply.github.com>
2020-12-19 14:21:57 +0100

    init
    added in b1.0.0

---
+++ Modules/Utilities.py
@@ -0,0 +1,23 @@
+class colors:
+    HEADER = '\033[95m'
+    WHITE = '\033[0m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    DARK_CYAN = '\033[90m'
+
+class utilities:
+    def Question(self, text):
+        print(colors.OKCYAN + "[" + colors.OKBLUE + colors.BOLD + "?" + colors.ENDC + colors.OKCYAN + "]" + " " + colors.ENDC + colors.DARK_CYAN + text + " Answer with Y (means yes) or N (means no): [Y/N]" + colors.ENDC, end=" ")
+        answer = input()
+        if answer == "" or answer == "y" or answer == "yes" or answer == "Y":
+            return True
+        elif answer == "n" or answer == "no" or answer == "N":
+            return False
+        else:
+            utilities.Question(0, text)
